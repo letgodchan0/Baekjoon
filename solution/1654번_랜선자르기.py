010eba8096ffb7fe105a73aaa5e7c6b31f01f779
letgodchan0 <letgodchan0@gmail.com>
2022-01-29 00:04:12 +0900

    랜선 자르기

--- solution/1654번_랜선자르기.py
+++ solution/1654번_랜선자르기.py
@@ -1,13 +1,35 @@
+# 시간 초과 코드
+
+# K, N = map(int, input().split())
+
+# lengths = []
+# for _ in range(K):
+#     lengths.append(int(input()))
+
+# tmp = sum(lengths) // N
+# while True:
+#     total = sum([length // tmp for length in lengths])
+#     if total >= N:
+#         print(tmp)
+#         break
+#     tmp -= 1
+
+
 K, N = map(int, input().split())
 
 lengths = []
 for _ in range(K):
     lengths.append(int(input()))
+    
+start, end = 1, max(lengths)
 
-tmp = sum(lengths) // N
 while True:
-    total = sum([length // tmp for length in lengths])
-    if total >= N:
-        print(tmp)
+    mid = (start + end) // 2
+    if start > end:
+        print(mid)
         break
-    tmp -= 1
+    count = [length // mid for length in lengths]
+    if sum(count) < N:
+        end = mid - 1
+    else:
+        start = mid + 1
