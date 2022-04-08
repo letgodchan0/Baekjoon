91c3e5dfa67d95dbcff4b00f36a58e08444f2f2f
letgodchan0 <letgodchan0@gmail.com>
2022-04-08 22:11:56 +0900

    2xn 타일링

--- solution/11726번_2xn 타일링.py
+++ solution/11726번_2xn 타일링.py
@@ -1,6 +1,6 @@
 number = int(input())
-lst = [0] * 1001
-lst[1], lst[2] = 1, 2
+dp = [0] * 1001
+dp[1], dp[2] = 1, 2
 for num in range(3, number+1):
-    lst[num] = lst[num-2] + lst[num - 1]
-print(lst[number]%10007)
+    dp[num] = dp[num-2] + dp[num - 1]
+print(dp[number]%10007)
