792efaec05ddb184bf22a764aca27918439c74c7
letgodchan0 <letgodchan0@gmail.com>
2022-10-15 18:40:17 +0900

    두 용액

---
+++ solution/2470번_두 용액.py
@@ -0,0 +1,18 @@
+import sys
+n = int(input())
+numbers = list(map(int, sys.stdin.readline().split()))
+numbers.sort()
+left, right, res = 0, n-1, sys.maxsize
+ans1, ans2 = 0, 0
+
+while left < right:
+    if abs(numbers[left] + numbers[right]) < res:
+        res = abs(numbers[left] + numbers[right])
+        ans1, ans2 = numbers[left], numbers[right]
+
+    if numbers[left] + numbers[right] < 0:
+        left += 1
+    else:
+        right -= 1
+
+print(ans1, ans2)
