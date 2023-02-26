05d847d5c1dfefb69078b6ac12371c36ba841ce7
letgodchan0 <letgodchan0@gmail.com>
2023-02-26 21:06:28 +0900

    1998년생인 내가 태국에서는 2541년생?!

---
+++ solution/1998년생인 내가 태국에서는 2541년생.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        System.out.println(num - 543);
+        
+    }
+}
